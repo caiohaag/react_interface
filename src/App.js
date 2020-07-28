6715b0c257a6cc2f0c32962542232cadd0e60b56
Caio Haag <caiohaag@gmail.com>
2020-07-28 13:45:26 -0300

    setup aula1

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import Menu from './components/Menu';
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
+import dadosIniciais from './data/dados_iniciais.json';
+
+function App() {
+  return (
+    <div style={{ background: "#141414"}}>
+      <Menu />
+
+      <BannerMain 
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"Corrida com polêmicas e domínio da Mercedes."}
+      />
+
+      <Carousel 
+        ignoreFistVideo
+        category={dadosIniciais.categorias[0]}
+      />
+
+      <Carousel 
+        ignoreFistVideo
+        category={dadosIniciais.categorias[1]}
+      />
+
+      <Carousel 
+        ignoreFistVideo
+        category={dadosIniciais.categorias[2]}
+      />
+
+      <Carousel 
+        ignoreFistVideo
+        category={dadosIniciais.categorias[3]}
+      />
+
+      <Carousel 
+        ignoreFistVideo
+        category={dadosIniciais.categorias[4]}
+      />
+
+      <Carousel 
+        ignoreFistVideo
+        category={dadosIniciais.categorias[5]}
+      />
+
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { FooterBase } from './styles';
+
+function Footer() {
+  return (
+    <FooterBase>
+      <a href="https://www.github.com/caiohaag" target="blank_">
+        <img className="caio" src="https://fontmeme.com/permalink/200728/da7b47c34c263e38c03f62c11d9894bf.png" alt="Logo Alura" />
+      </a>
+      <p>
+        51 98230.8080        
+      </p>
+    </FooterBase>
+  );
+}
+
+export default Footer;
