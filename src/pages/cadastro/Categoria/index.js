f9b0d066fbcef7a68bb5b788270bcfc55a7ad8b0
Caio Haag <caiohaag@gmail.com>
2020-07-31 18:40:44 -0300

    Update index.js

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -30,7 +30,9 @@ function CadastroCategoria() {
 
 
   useEffect(() => {
-    const URL_TOP = 'https://sportsflix.herokuapp.com/categorias';
+    const URL_TOP = window.location.hostname.includes('localhost')
+    ? 'http://localhost:8080/categorias'
+    : 'https://sportsflix.herokuapp.com/categorias'
     fetch(URL_TOP)
     .then(async (respostaDoServidor) => {
       const resposta = await respostaDoServidor.json();
